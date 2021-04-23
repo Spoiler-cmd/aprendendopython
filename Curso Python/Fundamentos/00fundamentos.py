34ca520f66ed330d915de27a323b59c1b5d3bd30
Yuri John <60278461+Spoiler-cmd@users.noreply.github.com>
2021-04-23 16:07:53 -0300

    Delete Curso Python directory

--- Curso Python/Fundamentos/00fundamentos.py
+++
@@ -1,5 +0,0 @@
-print('Primeiro Programa')
-print(1 + 2)
-
-
-print(1 + 2 + 3)
--- Curso Python/Fundamentos/01tiposbasicos.py
+++
@@ -1,8 +0,0 @@
-print(True)
-print(False)
-print(1.2 + 1)
-print('Aqui eu falo na minha lingua!')
-print('você é ' + 3 * 'muito ' + 'legal')
-#print(3 + '3') erro
-print([1, 2, 3])
-print({'nome': 'yuri', 'idade': 21})
